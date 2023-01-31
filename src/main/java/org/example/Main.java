d433b8f905d4337f69866118c4539a08b2b46169
Veeramani4156 <veeramani4156@gmail.com>
2023-01-31 13:10:02 +0530

    multi level inheritance

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,13 +9,20 @@ class AdvCalculator extends Calculator{
         return i + j;
     }
 }
+class NewCalculator extends AdvCalculator{
+    public int mul(int i, int j){
+        return i*j;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        AdvCalculator object = new AdvCalculator();
+        NewCalculator object = new NewCalculator();
         int result1 = object.add(13,12);
         int result2 = object.sub(76,23);
+        int result3 = object.mul(23, 13);
         System.out.println("addition of two number is: " + result1);
         System.out.println("Subtraction of two number is :" + result2);
+        System.out.println("multiplication of two number is :" + result3);
     }
 }
